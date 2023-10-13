106bdb1a8755568769519b1fefb802464de29cd3
yg <eng.yg@yahoo.com>
2023-10-13 13:31:14 +0800

    Refactor Header component

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,3 +1,9 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,7 +15,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course}/>
       <p>
         {part1} {exercises1}
       </p>
