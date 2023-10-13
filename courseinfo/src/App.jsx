bd08025d6aae3254005a4b5c09a5369c5924f97a
yg <eng.yg@yahoo.com>
2023-10-13 21:31:12 +0800

    Refactor Total component

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,6 +4,12 @@ const Header = (props) => {
   )
 }
 
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -25,7 +31,7 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
